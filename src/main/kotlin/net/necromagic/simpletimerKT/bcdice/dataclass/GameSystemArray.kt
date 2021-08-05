fe03838bed52bcff23dbbad764e95c7e70bfd854
JanMaki <shibashiba2002@gmail.com>
2021-08-05 14:32:16 +0900

    FirstCommit

---
+++ src/main/kotlin/net/necromagic/simpletimerKT/bcdice/dataclass/GameSystemArray.kt
@@ -0,0 +1,44 @@
+package net.necromagic.simpletimerKT.bcdice.dataclass
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * [/v2/game_system](https://github.com/bcdice/bcdice-api/blob/master/docs/api_v2.md#game-system)
+ *
+ * @property game_system [Array] GameSystemの一覧[GameSystem]
+ */
+@Serializable
+data class GameSystemArray(
+    @SerialName("game_system")
+    val game_system: Array<GameSystem>
+) {
+    /*
+    /**
+     * Equalsの実装
+     *
+     * @param other [Any] 比較対象
+     * @return [Boolean] 比較の結果
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GameSystemArray
+
+        if (!game_system.contentEquals(other.game_system)) return false
+
+        return true
+    }
+
+    /**
+     * HashCodeの生成の実装
+     *
+     * @return [Int] 生成結果
+     */
+    override fun hashCode(): Int {
+        return game_system.contentHashCode()
+    }
+
+     */
+}
